be39d12d08146410a0794ca87c3131b852e49501
manya02 <manyakr0@gmail.com>
2022-05-23 00:32:27 +0530

    code

--- project/app/urls.py
+++ project/app/urls.py
@@ -3,5 +3,5 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('index/', views.index,name='index'),
+    path('', views.index,name='index'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,14 @@
+from multiprocessing import context
 from django.shortcuts import render,HttpResponse
-
+from . models import Contact
 def index(request):
+    if request.method == 'POST':
+        name= request.POST["name"]
+        email=request.POST["email"]
+        ship=request.POST["ship"]
+        context = Contact(name=name,email=email,ship=ship)
+        context.save()
     return render(request,'app/index.html')
+    
 
 # Create your views here.
