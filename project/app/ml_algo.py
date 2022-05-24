3dc0e99c3554dd3b7419675224ec7fa88f33c771
Mohit Talreja <mohit.talreja@fero.ai>
2022-05-25 00:15:21 +0530

    Explanation for image url and image path

--- project/app/ml_algo.py
+++ project/app/ml_algo.py
@@ -1,90 +1,50 @@
-from django.shortcuts import render
-from app.models import Contact, ImageUpload
-from app.forms import ImageFrom
-from app.ml_algo import show_image
-from django.conf import settings
-import os
-
-
-def index(request):
-    if request.method == 'POST':
-        name = request.POST["name"]
-        email =request.POST["email"]
-        ship = request.POST["ship"]
-        pro = request.POST["pro"]
-        context = Contact(name=name,email=email,ship=ship,pro=pro)
-        context.save()
-    return render(request, 'app/index.html')
-    
-
-# Create your views here.
-def uploader(request):
-    context = {'form': ImageFrom()}
-    if request.method == 'POST':
-        form = ImageFrom(request.POST, request.FILES)
-        if form.is_valid():
-            image = form.save()
-            relative_path = image.photo.path     # if want to sent to your ml_algorithm
-            absolute_path = os.path.join(settings.BASE_DIR, relative_path)
-            show_image(path=absolute_path)
-    else:
-        image = ImageUpload.objects.last()      # returns last object of type ImageUpload
-        url = image.photo.url                   # while sending to html
-        context['url'] = url
-    return render(request, "app/test.html", context)
-
-
-
-
-
-
 # import tensorflow
 # from tensorflow.keras.preprocessing import image
 # from tensorflow.keras.layers import GlobalMaxPooling2D
 # from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
 # import pickle
-
-
+#
+#
 # import numpy as np
 # from numpy.linalg import norm
 # from sklearn.neighbors import NearestNeighbors
 # import cv2
-
+#
 # model = ResNet50(weights = 'imagenet',include_top=False, input_shape= (224,224,3))
 # model.trianable=False
 # model = tensorflow.keras.Sequential([
 #     model,
 #     GlobalMaxPooling2D()
 # ])
-
-
+#
+#
 # img = image.load_img('samples/bag.jpg', target_size = (224,224))
 # img_array = image.img_to_array(img)
 # expanded_img_array = np.expand_dims(img_array, axis = 0)
 # preprocessed_img = preprocess_input(expanded_img_array)
 # result = model.predict(preprocessed_img).flatten()
 # normal_result = result/norm(result)
-
-
-# def generate_img (details_file, names_file,n):
+#
+#
+# def generate_img(details_file, names_file,n):
 #     features_list = pickle.load(open(details_file, 'rb'))
 #     filenames = pickle.load(open(names_file ,'rb'))
-
+#
 #     neighbors = NearestNeighbors(n_neighbors = n , algorithm = 'brute', metric = 'euclidean')
 #     neighbors.fit(features_list)
-
+#
 #     distances , indices = neighbors.kneighbors([normal_result])
-
+#
 #     for file in indices[0][1:len(indices[0])]:
 #         temp_img = cv2.imread(filenames[file])
 #         cv2.imshow('output',cv2.resize(temp_img,(100,100)))
 #         cv2.waitKey(0)
-
+#
 # generate_img('feature_details.pkl','filenames.pkl',5)
-
+#
 # # flag = input("enter w for women, m  for men, else o ",)
-
-
+#
+#
 # if flag=='w':
 #     generate_img('feature_details_jeans.pkl','filenames_jeans.pkl',2)
 #     generate_img('feature_details_sandals.pkl','filenames_sandals.pkl',2)
@@ -96,8 +56,19 @@ def uploader(request):
 #         generate_img('feature_details_spec.pkl','filenames_spec.pkl',2)
 
 
+import cv2 as cv
+
+
+def show_image(path):
+    image = cv.imread(filename=path)
+    print(path)
+    # cv.imshow("MY IMAGE", image)
+    # cv.waitKey(0)
+    # cv.destroyAllWindows()
 
 
 
-
-
+'''
+html <- image.photo.url
+gen_image <- image.photo.path
+'''
--- project/app/models.py
+++ project/app/models.py
@@ -1,20 +1,18 @@
-from asyncio.windows_events import NULL
-from pyexpat import model
-from unicodedata import name
 from django.db import models
 
+
 class Contact (models.Model):
-    name=models.CharField(max_length=50)
-    email=models.EmailField()
-    ship=models.TextField()
-    pro=models.CharField(max_length=2,default="P1")
+    name = models.CharField(max_length=50)
+    email = models.EmailField()
+    ship = models.TextField()
+    pro = models.CharField(max_length=2,default="P1")
 
     def __str__(self):
         return self.name
 
 
-class img_upload(models.Model):
-    photo = models.ImageField(upload_to="my_img")
-    
+class ImageUpload(models.Model):
+    photo = models.ImageField(upload_to="images")
+
 
 # Create your models here.
